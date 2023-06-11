1f60042ea790c2c406a125b2067aa562a58b64fc
calvin <ncalvin67@gmail.com>
2023-06-11 07:09:01 +0300

    :sparkles: Navbar component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import Navbar from "./components/Navbar"
+
 const App = () => {
   return (
     <div className="w-screen h-screen">
-      My Name is Calvin
+      <Navbar />
     </div>
   )
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,20 @@
+const Navbar = () => {
+  return (
+    <div className="flex justify-between w-[95%] m-auto">
+        <ul className="flex mr-5">
+            <li>k</li>
+            <li>o</li>
+            <li>d</li>
+            <li>o</li>
+        </ul>
+        <div className="d">
+            <img src="vite.svg" alt="logo" />
+        </div>
+        <div>
+            <button>Contact</button>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
