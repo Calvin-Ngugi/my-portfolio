74f2c9d37e627ecbf9503e91bed2a00b80064203
calvin <ncalvin67@gmail.com>
2023-08-21 11:14:06 +0300

    :bento: Lunar project added

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,4 +1,4 @@
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 const Projects = ({ isDark }: any) => {
   return (
     <motion.div
@@ -11,6 +11,55 @@ const Projects = ({ isDark }: any) => {
     >
       <h1 className="text-center mb-5 font-bold text-[32px]">Projects</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3">
+        <div
+          className={`p-4 border-[2px] rounded-xl flex flex-col items-center ${
+            isDark
+              ? `bg-slate-950 border-gray-800`
+              : `bg-slate-200 border-gray-200`
+          }`}
+        >
+          <img src="lunar.PNG" alt="" className="h-[60%] mb-3" />
+          <h2 className="text-center mb-2 font-semibold">
+            Lunar Beauty Parlour and spa
+          </h2>
+          <p className="text-center">
+            This a marketing webite I made for a beauty shop called Lunar Beauty
+            Parlour and spa. It was created using react and the animation done
+            using framer motion.
+          </p>
+          <div className="w-[100%] flex justify-around mt-3">
+            <button
+              className={`cursor-pointer flex items-center justify-between mt-7 ps-4 pe-4 pt-2 pb-2 sm:p-2 border-2 rounded-xl ${
+                isDark
+                  ? `border-white hover:bg-slate-100 hover:text-slate-900 hover:font-semibold`
+                  : `border-black hover:bg-slate-900 hover:text-slate-100 hover:font-semibold`
+              }`}
+            >
+              <a
+                href="https://beauty-parlor.vercel.app/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                View Live
+              </a>
+            </button>
+            <button
+              className={`cursor-pointer flex items-center justify-between mt-7 ps-4 pe-4 pt-2 pb-2 sm:p-2 border-2 rounded-xl ${
+                isDark
+                  ? `border-white text-green-600 hover:bg-slate-100 hover:text-blue-950 hover:font-semibold`
+                  : `border-black text-blue-950 hover:bg-slate-900 hover:text-green-600 hover:font-semibold`
+              }`}
+            >
+              <a
+                href="https://github.com/Calvin-Ngugi/beauty-parlor"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Code
+              </a>
+            </button>
+          </div>
+        </div>
         <div
           className={`p-4 border-[2px] rounded-xl flex flex-col items-center ${
             isDark
@@ -117,7 +166,8 @@ const Projects = ({ isDark }: any) => {
           <h2 className="text-center mb-2 font-semibold">Techware Ent. Ltd.</h2>
           <p className="text-center">
             This is an ecommerce website that I build for a company called
-            Techware Enterprises Ltd. It is created using wordpress and the
+            Techware Enterprises Ltd. They deal in computer devices and
+            surveillance equipment. The site is created using wordpress and the
             payment system uses PayPal.
           </p>
           <div className="w-[100%] flex justify-around mt-3">
@@ -151,7 +201,13 @@ const Projects = ({ isDark }: any) => {
             Connectify is a social platform that allows users to read posts and
             follow users. To access all features one needs to be a premium
             membership. Login details are the username and zipcode for{" "}
-            <a href="https://jsonplaceholder.typicode.com/users" className='font-semibold hover:font-bold hover:underline'>users</a>.
+            <a
+              href="https://jsonplaceholder.typicode.com/users"
+              className="font-semibold hover:font-bold hover:underline"
+            >
+              users table
+            </a>
+            .
           </p>
           <div className="w-[100%] flex justify-around mt-3">
             <button
